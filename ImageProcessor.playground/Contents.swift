bdda753dbd71d90d083a59b5e366fc127bf6bd97
eldaroid <eldaroid@MacBook-Air-Eldar.local>
2020-12-20 05:56:22 +0300

    final correction

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 let image = UIImage(named: "switzerland.jpg")!
-
-// Process the image!
 var myRGBA = RGBAImage(image: image)!
+
+//This part of the code is responsible for calculating the average values of Red Green and Blue, which we later use for changes.
 
 var totalRed = 0
 var totalGreen = 0
@@ -25,49 +25,20 @@ let avgRed = totalRed/count
 let avgGreen = totalGreen/count
 let avgBlue = totalBlue/count
 
-//let avgRed = 68
-//let avgGreen = 81
-//let avgBlue = 82
-//
-//for y in 0..<myRGBA.height{
-//    for x in 0..<myRGBA.width{
-//        let index = y * myRGBA.width + x
-//        var pixel = myRGBA.pixels[index]
-//        let redDiff = Int(pixel.red) - avgRed
-//        if (redDiff > 0)
-//        {
-//            pixel.red = UInt8( max(0,min(255,avgRed+redDiff * 5 ) ) )
-//            myRGBA.pixels[index] = pixel
-//        }
-//    }
-//}
-//
-//let newImage2 = myRGBA.toUIImage()
+ //The next part of the code is responsible for changing the color settings of the photo.
+
+for y in 0..<myRGBA.height{
+    for x in 0..<myRGBA.width{
+        let index = y * myRGBA.width + x
+        var pixel = myRGBA.pixels[index]
+        let redDiff = Int(pixel.red) - avgRed
+        if (redDiff > 0)
+        {
+            pixel.red = UInt8( max(0,min(255,avgRed+redDiff * 5 ) ) )
+            myRGBA.pixels[index] = pixel
+        }
+    }
+}
+
+let newImage2 = myRGBA.toUIImage()
         
-
-
-
-
-
-
-
-
-
-
-
-
-//let x = 100
-//let y = 100
-//
-//let index = y * myRGBA.width + x
-//
-//var pixel = myRGBA.pixels[index]
-//
-//pixel.blue = 0
-//pixel.red = 0
-//pixel.green = 255
-//
-//myRGBA.pixels[index] = pixel
-//
-//let newImage = myRGBA.toUIImage()
-//newImage
